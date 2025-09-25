1f1e8aca960aabc044aea15ed40898479f81a1ec
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-09-25 01:01:34 +0100

    added basic socket connection functionality

--- socket.ts
+++ socket.ts
@@ -1,4 +1,26 @@
 import net from "net"
 
-const server =  net.createServer()
-server.listen("8080")
+const socket = net.createServer()
+
+function newConn(socket: net.Socket) {
+  console.log(`New connection from ${socket.remoteAddress}:${socket.remotePort}`)
+
+  socket.on("end", () => {
+    console.log("received FIN")
+  })
+
+  socket.on("data", (data: Buffer) => {
+    if (data.length > 0) {
+      socket.write(data)
+    }
+
+    if (data.includes("q")) {
+      console.log("closing...")
+      socket.end()
+    }
+  })
+}
+
+socket.on("connection", newConn)
+console.log("server listening on port 8080")
+socket.listen("8080")
