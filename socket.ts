423fbcd538257a030a7b8ba59e4bdbd3c579c157
Enoch Fagbenja <fagbenjaenoch73@gmail.com>
2025-09-25 01:50:02 +0100

    testing formatting on neovim

--- socket.ts
+++ socket.ts
@@ -1,26 +1,28 @@
-import net from "net"
+import net from "net";
 
-const socket = net.createServer()
+const socket = net.createServer();
 
 function newConn(socket: net.Socket) {
-  console.log(`New connection from ${socket.remoteAddress}:${socket.remotePort}`)
+	console.log(
+		`New connection from ${socket.remoteAddress}:${socket.remotePort}`,
+	);
 
-  socket.on("end", () => {
-    console.log("received FIN")
-  })
+	socket.on("end", () => {
+		console.log("received FIN");
+	});
 
-  socket.on("data", (data: Buffer) => {
-    if (data.length > 0) {
-      socket.write(data)
-    }
+	socket.on("data", (data: Buffer) => {
+		if (data.length > 0) {
+			socket.write(data);
+		}
 
-    if (data.includes("q")) {
-      console.log("closing...")
-      socket.end()
-    }
-  })
+		if (data.includes("q")) {
+			console.log("closing...");
+			socket.end();
+		}
+	});
 }
 
-socket.on("connection", newConn)
-console.log("server listening on port 8080")
-socket.listen("8080")
+socket.on("connection", newConn);
+console.log("server listening on port 8080");
+socket.listen("8080");
